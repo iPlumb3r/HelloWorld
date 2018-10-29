d2075d2a8cd00bdef8c672cce019e3a4b9edf293
babonet13 <admin@hubject.net>
2018-10-29 17:26:39 +0100

    Update install-docker.sh

--- SH-Script/install-docker.sh
+++ SH-Script/install-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Install Docker
-add-apt-repository \ "deb [arch=amd64] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) \ stable"
-apt-get update
-usermod -aG docker satoshi
+sudo add-apt-repository \ "deb [arch=amd64] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) \ stable"
+sudo apt-get update
+sudo sermod -aG docker satoshi
